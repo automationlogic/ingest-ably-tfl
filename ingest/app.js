df1022a16c957ae39d2e670364461aa7f0728847
Piotr Grzeskowiak <piotr@automationlogic.com>
2020-11-18 13:45:20 +0000

    GCP-109 added webserver and handlers to ingest
    Handlers for App Engine requests described here: https://cloud.google.com/appengine/docs/standard/python/how-instances-are-managed

--- ingest/app.js
+++ ingest/app.js
@@ -1,12 +1,16 @@
+'use strict';
+
+const express = require('express');
 const Ably = require('ably');
 const {PubSub} = require('@google-cloud/pubsub');
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
+const app = express();
 const projectId = process.env.PROJECT;
 const pubSub = new PubSub({projectId});
 const secretManager = new SecretManagerServiceClient({projectId});
 
-async function main(
+async function forwardMessages(
   topicName = process.env.TOPIC,
   subscriptionName = process.env.SUBSCRIPTION
 ) {
@@ -20,6 +24,7 @@ async function main(
   const channelName = '[product:ably-tfl/tube]tube:jubilee:940GZZLUWYP:arrivals';
   const channel = ablyClient.channels.get(channelName);
 
+  console.log('Subscribing to Ably TfL...');
   channel.subscribe(async function(message) {
     let buf = Buffer.from(JSON.stringify(message.data));
     console.log(`Forwarding ${message.data.length} records to Pub/Sub`);
@@ -29,4 +34,23 @@ async function main(
   });
 }
 
-main();
+app.get('/', (req, res) => {
+  res.status(200).send('ok').end();
+});
+
+// Dummy handler for App Engine request sent on startup
+app.get('/_ah/start', (req, res) => {
+  res.status(200).send('App Starting').end();
+});
+
+// Dummy handler for App Engine request sent on shutdown
+app.get('/_ah/stop', (req, res) => {
+  res.status(200).send('App Stopping').end();
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+forwardMessages();
